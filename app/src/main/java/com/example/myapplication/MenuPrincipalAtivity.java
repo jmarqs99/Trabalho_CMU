9f7e4e3503a429114480d472cc7fa6bb28c3ca4e
8180223 <8180223@estg.ipp.pt>
2021-01-27 16:33:03 +0000

    Quiz fragment

--- app/src/main/java/com/example/myapplication/MenuPrincipalAtivity.java
+++ app/src/main/java/com/example/myapplication/MenuPrincipalAtivity.java
@@ -28,16 +28,19 @@ import retrofit2.Response;
 
 public class MenuPrincipalAtivity extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
-    private EquipaAdapter mAdapter;
-    private Button logoutButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal_fragment);
 
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction tr = fm.beginTransaction();
 
+        QuizFragment newQFragment = new QuizFragment();
+
+        tr.replace(R.id.fragment2,newQFragment);
+        tr.addToBackStack(null);
+        tr.commit();
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -46,24 +49,26 @@ public class MenuPrincipalAtivity extends AppCompatActivity {
                 FragmentManager fm = getSupportFragmentManager();
                 FragmentTransaction tr = fm.beginTransaction();
                 switch (item.getItemId()) {
-                    case R.id.action_recents:
-                        Toast.makeText(MenuPrincipalAtivity.this, "Something", Toast.LENGTH_SHORT).show();
+                    case R.id.action_quizz:
+                        QuizFragment newQFragment = new QuizFragment();
+
+                        tr.replace(R.id.fragment2,newQFragment);
+                        tr.addToBackStack(null);
+                        tr.commit();
                         break;
-                    case R.id.action_favorites:
+                    case R.id.action_standings:
                         ClassificacoesFragment newFragment = new ClassificacoesFragment();
 
                         tr.replace(R.id.fragment2,newFragment);
                         tr.addToBackStack(null);
                         tr.commit();
-                        Toast.makeText(MenuPrincipalAtivity.this, "Classificações", Toast.LENGTH_SHORT).show();
                         break;
-                    case R.id.action_nearby:
+                    case R.id.action_profile:
                         PerfilFragment newPFragment = new PerfilFragment();
 
                         tr.replace(R.id.fragment2,newPFragment);
                         tr.addToBackStack(null);
                         tr.commit();
-                        Toast.makeText(MenuPrincipalAtivity.this, "Perfil", Toast.LENGTH_SHORT).show();
                         break;
                 }
                 return true;
---
+++ app/src/main/java/com/example/myapplication/QuizFragment.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class QuizFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.quizs_fragment, container, false);
+
+        return v;
+    }
+}
