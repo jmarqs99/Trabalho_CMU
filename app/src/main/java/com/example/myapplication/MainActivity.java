f5ca4eef99aafbdd58a992967577ce70e0beec55
Miguel <migueltomaz.c@gmail.com>
2021-02-02 15:48:14 +0000

    Login com a conta do google

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -26,15 +26,19 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -48,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements LoginSelected, Re
     private FirebaseFirestore db;
     private GoogleSignInClient signInClient;
     private JobScheduler mScheduler;
+    private final static int RC_SIGN_IN = 123;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -67,6 +72,8 @@ public class MainActivity extends AppCompatActivity implements LoginSelected, Re
         startService(intent);
 
         mAuth = FirebaseAuth.getInstance();
+
+        createRequest();
     }
 
     @Override
@@ -86,11 +93,13 @@ public class MainActivity extends AppCompatActivity implements LoginSelected, Re
 
                     FirebaseUser user = mAuth.getCurrentUser();
                     updateUI(user);
+                    /**
                     Intent intent = new Intent(MainActivity.this, MenuPrincipalAtivity.class);
                     intent.putExtra("mailUser", mail);
                     intent.putExtra("passUser", pass);
                     startActivity(intent);
                     finish();
+                     */
                     errorMessages.setText("Login bem sucedido");
                 }
                 else {
@@ -101,27 +110,71 @@ public class MainActivity extends AppCompatActivity implements LoginSelected, Re
         });
     }
 
-    @Override
-    public void onLoginWithGoogleSelected() {
-
+    private void createRequest() {
         GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
         signInClient = GoogleSignIn.getClient(this, signInOptions);
+    }
 
-        /**
-        Intent intent = new Intent(MainActivity.this, MenuPrincipalAtivity.class);
-        intent.putExtra("mailUser", signInOptions.getAccount());
-        startActivity(intent);
-        finish();
-         */
-       // Intent intent = signInClient.getSignInIntent();
-       // startActivityForResult(intent);
+    @Override
+    public void onLoginWithGoogleSelected() {
+
+        Intent intent = signInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+        Log.d("AquiTbm", "chegou aqui tbm");
 
     }
 
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                Log.d("Aqui", "chegou aqui");
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+
+    private void firebaseAuthWithGoogle(String idToken) {
+            AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+            mAuth.signInWithCredential(credential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("googleSucesso", "Login com google sucesso");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.d("googleFalhou", "Login com google falhou");
+                                updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+    }
 
 
     @Override
@@ -189,8 +242,20 @@ public class MainActivity extends AppCompatActivity implements LoginSelected, Re
 
     private void updateUI(FirebaseUser user) {
 
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+
         if(user != null) {
+            String mail = user.getEmail();
             Intent intent = new Intent(MainActivity.this, MenuPrincipalAtivity.class);
+            intent.putExtra("mailUser", mail);
+            startActivity(intent);
+            finish();
+        }
+        else if(account != null) {
+            String mail = account.getEmail();
+
+            Intent intent = new Intent(MainActivity.this, MenuPrincipalAtivity.class);
+            intent.putExtra("mailUser", mail);
             startActivity(intent);
             finish();
         }
